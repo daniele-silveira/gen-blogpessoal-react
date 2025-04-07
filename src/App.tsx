7c976943fcc766e4171f12b79a8922a8fba9f8e3
Daniele Napoleao <daniele20@gmail.com>
2025-04-07 11:12:39 -0300

    Routes/Rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import Home from './assets/pages/home/Home'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Footer from './components/footer/Footer';
+import Navbar from './components/navbar/Navbar';
+import Home from './pages/home/Home';
 
 function App() {
-    return (
-        <Home />
-    )
+  return (
+    <>
+      <BrowserRouter>
+          <Navbar />
+          <div className="min-h-[80vh]">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,34 @@
+import {GithubLogo, InstagramLogo, 
+    LinkedinLogo 
+} from '@phosphor-icons/react'
+
+function Footer() {
+
+    let data = new Date().getFullYear()
+
+    return (
+        <>
+            <div className="flex justify-center bg-indigo-900 text-white">
+                <div className="container flex flex-col items-center py-4">
+                    <p className='text-xl font-bold'>
+                            Blog Pessoal Generation | Copyright: {data}
+                        </p>
+                    <p className='text-lg'>Acesse nossas redes sociais</p>
+                    <div className='flex gap-2'>
+	                        <a href="https://www.linkedin.com/in/daniele-napoleao" target="_blank">
+    	                    <LinkedinLogo size={48} weight='bold' />
+                        </a>
+                            <a href="https://www.instagram.com" target="_blank">
+    	                    <InstagramLogo size={48} weight='bold' />
+                        </a>
+                            <a href="https://www.github.com/daniele-silveira" target="_blank">
+    	                    <GithubLogo size={48} weight='bold' />
+	                    </a>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Footer
